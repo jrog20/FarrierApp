deaf06fdf97d354ed1b8c97dd21453f73b93eadd
jrog20 <jaimerogstad@gmail.com>
2020-12-26 19:08:19 -0500

    Start building farrier controller

--- app/controllers/farriers_controller.rb
+++ app/controllers/farriers_controller.rb
@@ -1,2 +1,24 @@
 class FarriersController < ApplicationController
+  def new
+    @farrier = Farrier.new
+  end
+
+  def create
+    if @farrier = Farrier.create(farrier_params)
+      session[:farrier_id] = @farrier.id
+      redirect_to farrier_path(@farrier)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @farrier = Farrier.find_by_id(session[:farrier_id])
+  end
+
+  private
+
+  def farrier_params
+    params.require(:farrier).permit(:first_name, :last_name, username, :email, :password, :company_name)
+  end
 end
