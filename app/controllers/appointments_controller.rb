5e7a4c7075b160dcc2a97caf79dad7562434883e
jrog20 <jaimerogstad@gmail.com>
2020-12-28 17:39:29 -0500

    Start sketching appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,9 @@
 class AppointmentsController < ApplicationController
+  def index
+    @appointments = Appointment.all
+  end
+  
+  def show
+    @appointment = Appointment.find_by(params[:id])
+  end
 end
