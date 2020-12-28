2ea4cae9ae17a059b6d58d262fae1f7db3c8dbd3
jrog20 <jaimerogstad@gmail.com>
2020-12-28 12:44:08 -0500

    Build horse controller actions and initial new and show views

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,2 +1,20 @@
 class HorsesController < ApplicationController
+  def new
+    @horse = Horse.new
+  end
+
+  def create
+    @horse = Horse.create(horse_params)
+    redirect_to @horse
+  end
+
+  def show
+    @horse = Horse.find_by(params[:horse_id])
+  end
+
+  private
+
+  def horse_params
+    params.require(:horse).permit(:name, :needs_shoes, :front_shoes, :hind_shoes, :winter_shoes, :pads, :shoe_size, :temperament, :schedule, :comments)
+  end
 end
