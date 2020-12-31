05431b19f47bdc3a19f9cbbb09916a9b449765fd
jrog20 <jaimerogstad@gmail.com>
2020-12-31 13:54:18 -0500

    Move add horse and appt links from farrier show page to main nav bar

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,6 +1,10 @@
 class HorsesController < ApplicationController
   before_action :redirect_if_not_logged_in
   
+  def index
+    @horses = Horse.all
+  end
+
   def new
     @horse = Horse.new
   end
