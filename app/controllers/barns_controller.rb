81c2bb3bea93f939404f1b68fb45e82e2b49d362
jrog20 <jaimerogstad@gmail.com>
2021-01-04 16:44:06 -0500

    Add basic actions to new migrations

--- app/controllers/barns_controller.rb
+++ app/controllers/barns_controller.rb
@@ -1,2 +1,24 @@
 class BarnsController < ApplicationController
+  def new
+    @barn = Barn.new
+  end
+
+  #need to connect horses, barns, and owners at creation
+  def create
+    if @barn = Barn.create(barn_params)
+      redirect_to barn_path(@barn)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @barn = Barn.find_by_id(params[:id])
+  end
+
+  private
+
+  def barn_params
+    params.require(:barn).permit(:name, :manager_name, :phone, :email, :address, :city, :state, :zip_code, :comments)
+  end
 end
--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,24 @@
 class OwnersController < ApplicationController
+  def new
+    @owner = Owner.new
+  end
+
+  #need to connect horses, barns, and owners at creation
+  def create
+    if @owner = Owner.create(owner_params)
+      redirect_to owner_path(@owner)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @owner = Owner.find_by_id(params[:id])
+  end
+
+  private
+
+  def owner_params
+    params.require(:owner).permit(:first_name, :last_name, :phone, :email, :comments)
+  end
 end
