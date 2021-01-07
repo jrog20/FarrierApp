575ce9b8aa3e82b047ad2cbdd06f4dcce523fbde
jrog20 <jaimerogstad@gmail.com>
2021-01-07 16:02:38 -0500

    Add barn_name and owner_name getter and setters

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -7,4 +7,20 @@ class Horse < ApplicationRecord
   validates :name, presence: true
 
   scope :alpha, -> { order(:name) }
+
+  def barn_name=(name)
+    self.barn = Barn.find_or_create_by(name: name)
+  end
+
+  def barn_name
+     self.barn ? self.barn.name : nil
+  end
+
+  def owner_name=(name)
+    self.owner = Owner.find_or_create_by(name: name)
+  end
+
+  def owner_name
+     self.owner ? self.owner.name : nil
+  end
 end
